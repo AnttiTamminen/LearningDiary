20083580df4b37b2f30a7b6726a436740afb4ece
Antti <antti.a.tamminen@gmail.com>
2022-06-14 14:31:32 +0300

    copy from feature to main

--- Program.cs
+++ Program.cs
@@ -23,14 +23,165 @@ namespace LearningDiary
             }
 
             Console.WriteLine("Do you want to see list of all topics? (yes/no)");
-            string printList = Console.ReadLine().ToLower();
-            if (printList == "yes" && File.Exists(url) && new FileInfo(url).Length != 0)
+            answerToStart = Console.ReadLine().ToLower();
+            if (answerToStart == "yes" && File.Exists(url) && new FileInfo(url).Length != 0)
+            {
                 PrintTopics(url);
-            else if (printList == "yes")
+
+                //Console.WriteLine("Find a specific topic? (yes/no)");
+                //answerToStart = Console.ReadLine().ToLower();
+                //if (answerToStart == "yes")
+                //{
+                //    Console.WriteLine("Search with title give 1 search with id give 2");
+                //    ...
+                //}
+            }
+            else if (answerToStart == "yes")
             {
                 Console.Clear();
                 Console.WriteLine("No topics created yet. Restart program to give first topic entry!");
             }
+        }
+
+        public static List<Topic> FileTxtToTopiclist(string url)
+        {
+            string[] fileTextArray = File.ReadAllText(url).Split("###");
+            fileTextArray = fileTextArray.Take(fileTextArray.Length - 1).ToArray(); //removes last empty element from array (I need to change the file structure at some point
+            List<Topic> topicList = new List<Topic>();
+            int headingLength;
+            for (int i = 0; i < fileTextArray.Length; i++)
+            {
+                string[] topicFieldsArray = fileTextArray[i].Split("##");
+                topicList.Add(new Topic());
+
+                headingLength = 4;
+                topicList[i].Id = Convert.ToInt32(topicFieldsArray[0].Substring(headingLength,
+                    topicFieldsArray[0].Length - headingLength));
+
+                headingLength = 7;
+                topicList[i].Title = topicFieldsArray[1].Substring(headingLength,
+                    topicFieldsArray[1].Length - headingLength);
+
+                headingLength = 13;
+                topicList[i].Description = topicFieldsArray[2].Substring(headingLength,
+                    topicFieldsArray[2].Length - headingLength);
+
+                headingLength = 23;
+                string etaMasterString = topicFieldsArray[3].Substring(headingLength,
+                    topicFieldsArray[3].Length - headingLength);
+                if (!String.IsNullOrEmpty(etaMasterString))
+                    topicList[i].EstimatedTimeToMaster = Convert.ToDouble(etaMasterString);
+
+                headingLength = 11;
+                string timeSpentString = topicFieldsArray[4].Substring(headingLength,
+                    topicFieldsArray[4].Length - headingLength);
+                if (!String.IsNullOrEmpty(timeSpentString))
+                    topicList[i].TimeSpent = Convert.ToDouble(timeSpentString);
+
+                headingLength = 8;
+                topicList[i].Source = topicFieldsArray[5].Substring(headingLength,
+                    topicFieldsArray[5].Length - headingLength);
+
+                headingLength = 19;
+                string startDateString = topicFieldsArray[6].Substring(headingLength,
+                    topicFieldsArray[6].Length - headingLength);
+                if (!String.IsNullOrEmpty(startDateString))
+                    topicList[i].StartLearningDate = Convert.ToDateTime(startDateString);
+
+                headingLength = 12;
+                topicList[i].InProgress = Convert.ToBoolean(topicFieldsArray[7].Substring(headingLength,
+                    topicFieldsArray[7].Length - headingLength));
+
+                headingLength = 16;
+                string completeDateString = topicFieldsArray[8].Substring(headingLength,
+                    topicFieldsArray[8].Length - headingLength);
+                if (!String.IsNullOrEmpty(completeDateString))
+                    topicList[i].CompletionDate = Convert.ToDateTime(completeDateString);
+
+                if (topicFieldsArray[9].Contains("*TASK*"))
+                    topicList[i].TaskList = FileTxtToTasklist(topicFieldsArray[9]);
+            }
+            return topicList;
+        }
+
+        public static List<Task> FileTxtToTasklist(string tasksFromFile)
+        {
+            List<Task> taskList = new List<Task>();
+            string[] taskArray = tasksFromFile.Split("*TASK*");
+
+            if (taskArray.Any())
+            {
+                int headingLength;
+                for (int i = 1; i < taskArray.Length; i++) //taskArray first element is null so i starts from one
+                {
+                    taskList.Add(new Task());
+                    string[] taskFieldsArray = taskArray[i].Split("+++");
+
+                    if (taskFieldsArray.Any())
+                    {
+                        headingLength = 4;
+                        string idString = taskFieldsArray[0].Substring(headingLength,
+                            taskFieldsArray[0].Length - headingLength);
+                        if (!String.IsNullOrEmpty(idString))
+                            taskList[i-1].Id = Convert.ToInt32(idString);
+
+                        headingLength = 7;
+                        taskList[i-1].Title = taskFieldsArray[1].Substring(headingLength,
+                            taskFieldsArray[1].Length - headingLength);
+
+                        headingLength = 13;
+                        taskList[i - 1].Description = taskFieldsArray[2].Substring(headingLength,
+                            taskFieldsArray[2].Length - headingLength);
+
+                        headingLength = 6;
+                        taskList[i - 1].Notes = new List<string>(taskFieldsArray[3].Substring(headingLength,
+                            taskFieldsArray[3].Length - headingLength).Trim().Split(' '));
+
+                        headingLength = 10;
+                        string deadlineString = taskFieldsArray[4].Substring(headingLength,
+                            taskFieldsArray[4].Length - headingLength);
+                        if (!String.IsNullOrEmpty(deadlineString))
+                            taskList[i - 1].Deadline = Convert.ToDateTime(deadlineString);
+
+                        headingLength = 10;
+                        string taskPriority = taskFieldsArray[5].Substring(headingLength,
+                            taskFieldsArray[5].Length - headingLength);
+                        if (taskPriority == "Low")
+                            taskList[i - 1].Priority = Task.EnumPriority.Low;
+                        else if (taskPriority == "Medium")
+                            taskList[i - 1].Priority = Task.EnumPriority.Medium;
+                        else if (taskPriority == "High")
+                            taskList[i - 1].Priority = Task.EnumPriority.High;
+
+                        headingLength = 6;
+                        taskList[i - 1].Done = Convert.ToBoolean(taskFieldsArray[6].Substring(headingLength,
+                            taskFieldsArray[6].Length - headingLength));
+                    }
+                }
+            }
+            return taskList;
+        }
+
+        public static void FindTopicByTitle(string url, string searchTitle)
+        {
+            List <Topic> topicList = FileTxtToTopiclist(url);
+
+            //if (File.Exists(url) && new FileInfo(url).Length != 0)
+            //{
+
+            //    // Creating Dictionary of Topics by title
+            //    Dictionary<string, string[]> topicDictionary = new Dictionary<string, string[]>();
+            //    string[] fileTextAsArray = File.ReadAllText(url).Split("###");
+            //    int titleHeadingLenght = 5;
+            //    for (int i = 0; i < fileTextAsArray.Length - 1; i++)
+            //    {
+            //        string[] topicFieldsArray = fileTextAsArray[i].Split("##");
+            //        string titleKey = topicFieldsArray[1].Substring(titleHeadingLenght, topicFieldsArray[1].Length - titleHeadingLenght + 1);
+            //        if (titleKey != " ")
+            //            topicDictionary.Add(titleKey, topicFieldsArray);
+            //    }
+
+            //}
         }
 
         public static void Welcome()
@@ -43,18 +194,52 @@ namespace LearningDiary
         public static void PrintTopics(string url)
         {
             Console.Clear();
-            List<string> diaryContents = File.ReadAllText(url).Split("###").ToList(); // ### used to recognize Topic end
-            diaryContents.RemoveAt(diaryContents.Count - 1);
-            List<string> printTextList = new List<string>();
-            for (int i = 0; i < diaryContents.Count; i++)
+            List<Topic> readTopics = FileTxtToTopiclist(url);
+            for (int i = 0; i < readTopics.Count; i++)
             {
-                printTextList = diaryContents[i].Split("##").ToList(); // ## used to recognize Topic properties end
-                for (int j = 0; j < printTextList.Count; j++)
-                    Console.WriteLine(printTextList[j]);
-
-                Console.WriteLine();
-                Console.WriteLine("--------------------------------------------------------------"); // --------- Used to divide Topics in console window
+                foreach (var value in readTopics[i].GetType().GetProperties())
+                {
+                    if (value.Name == "TaskList" && readTopics[i].TaskList != null)
+                    {
+                        Console.WriteLine($"\n{value.Name}:\n");
+                        PrintTasks(readTopics[i].TaskList);
+                    }
+                    else 
+                        Console.WriteLine($"{value.Name}: {value.GetValue(readTopics[i])}");
+                }
+                Console.WriteLine("\n------------------------------------------------------\n");
             }
+        }
+
+        public static void PrintTasks(List<Task> taskToPrint)
+        {
+            for (int i = 0; i < taskToPrint.Count; i++)
+            {
+                foreach (var value in taskToPrint[i].GetType().GetProperties())
+                {
+                    if (value.Name == "Notes" && taskToPrint[i].Notes != null)
+                    {
+                        Console.WriteLine($"{value.Name}:");
+                        PrintNotes(taskToPrint[i].Notes);
+                    }
+                    else 
+                        Console.WriteLine($"{value.Name}: {value.GetValue(taskToPrint[i])}");
+                }
+                if (i < taskToPrint.Count -1)
+                    Console.WriteLine("\n+++++++++++++++++++++++++++\n");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintNotes(List<string> note)
+        {
+            for (int i = 1; i < note.Count+1; i++)
+            {
+                if (i % 13 == 0)
+                    Console.WriteLine();
+                Console.Write($"{note[i - 1]} ");
+            }
+            Console.WriteLine();
         }
 
         public static void TopicsToTxtfile(List<Topic> createdTopics, string url)
@@ -63,8 +248,11 @@ namespace LearningDiary
             {
                 foreach (var value in createdTopics[i].GetType().GetProperties()) 
                 {
-                    if (value.Name == "TaskList" && createdTopics[i].TaskList != null)
+                    if (value.Name == "TaskList" && createdTopics[i].TaskList?.Any() == true)
+                    {
                         TasksToTxtfile(createdTopics[i], url);
+                        File.AppendAllText(url, "##");
+                    }
                     else
                         File.AppendAllText(url, string.Format($"{value.Name.ToUpper()}: {value.GetValue(createdTopics[i])}##"));
                 }
@@ -73,23 +261,23 @@ namespace LearningDiary
             }
         }
 
-        public static void TasksToTxtfile(Topic oneTopic, string url) 
+        public static void TasksToTxtfile(Topic oneTopic, string url)
         {
-            List<Task> listOfTasks = oneTopic.TaskList;  
+            File.AppendAllText(url, "TaskList: ");
+            List <Task> listOfTasks = oneTopic.TaskList;  
             for (int j = 0; j < listOfTasks.Count; j++)
             {
-                File.AppendAllText(url, string.Format($"\n*TASK* ( \n"));
+                File.AppendAllText(url, "*TASK*");
                 foreach (var item in listOfTasks[j].GetType().GetProperties())
                 {
-                    if (item.Name == "Notes" && listOfTasks[j].Notes != null)
+                    if (item.Name == "Notes" && listOfTasks[j].Notes?.Any() == true)
                     {
-                        File.AppendAllText(url, string.Format($"{item.Name.ToUpper()}:##"));
+                        File.AppendAllText(url, string.Format($"{item.Name.ToUpper()}: "));
                         NotesToTxtfile(listOfTasks[j], url);
                     }
                     else
-                        File.AppendAllText(url, string.Format($"{item.Name.ToUpper()}: {item.GetValue(listOfTasks[j])}##"));
+                        File.AppendAllText(url, string.Format($"{item.Name.ToUpper()}: {item.GetValue(listOfTasks[j])}+++"));
                 }
-                File.AppendAllText(url, ")##");
             }
         }
 
@@ -98,23 +286,19 @@ namespace LearningDiary
             for (int i = 0; i < oneTask.Notes.Count; i++)
             {
                 File.AppendAllText(url, string.Format($"{oneTask.Notes[i]} "));
-                if (i % 10 == 0 && i != 0 && i != oneTask.Notes.Count - 1)
-                {
-                    File.AppendAllText(url, "\n");
-                }
             }
-            File.AppendAllText(url, "##");
+            File.AppendAllText(url, "+++");
         }
 
         public static List<Topic> CreateTopics(string answerToStart, string url)
         {
             int nextId;
 
-            //Hakee viimeisen Topic idn tiedostosta
+            //Hakee viimeisen Topic Idn tiedostosta
             if (File.Exists(url) && new FileInfo(url).Length != 0)
             {
-                string[] fielTextAsArray = File.ReadAllText(url).Split("###");
-                string lastTopicStr = fielTextAsArray[fielTextAsArray.Length - 2];
+                string[] fileTextAsArray = File.ReadAllText(url).Split("###");
+                string lastTopicStr = fileTextAsArray[fileTextAsArray.Length - 2];
                 int idHeaderLength = 4; // maaginen 4 tulee koska "ID: " on neljä merkkiä
                 nextId = Convert.ToInt32(lastTopicStr.Substring(idHeaderLength, lastTopicStr.IndexOf('#') - idHeaderLength)) + 1;
             }
@@ -267,7 +451,7 @@ namespace LearningDiary
                     {
                         taskDeadline = Console.ReadLine();
                         if (!String.IsNullOrEmpty(taskDeadline))
-                            taskList[taskList.Count - 1].Deadline = Convert.ToDateTime(Console.ReadLine());
+                            taskList[taskList.Count - 1].Deadline = Convert.ToDateTime(taskDeadline);
                         break;
                     }
                     catch (Exception)
